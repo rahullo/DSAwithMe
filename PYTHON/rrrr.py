8a43902bc71c87badcd5009f39fd38b629baec5e
Rahul Lohra <rahullohra987@gmail.com>
2022-07-06 22:23:33 +0530

    asdjfa

--- PYTHON/rrrr.py
+++ PYTHON/rrrr.py
@@ -1,18 +1,10 @@
-def twoSum(numbers, target):
-  l = 0
-  r = len(numbers)-1
-  while l<r:
-    sum = numbers[l] + numbers[r]
-    if sum == target:
-      return [l+1, r+1]
-    elif sum < target:
-      l+=1
-    else:
-      r-=1
+from collections import deque
+# import queue
 
-print(twoSum([0, 0, 11, 15], 0))
-
-# li = [1, 2, 3, 4, 5]
-# ans = li[2:]
-
-# print(ans)
+queue = deque(['rahul', 'chandan', 'yashwant'])
+print(queue)
+queue.append('gautam')
+print(queue)
+fifo = queue.popleft()
+print(fifo)
+print(queue)
