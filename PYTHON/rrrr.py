5a2262870ceb747a0885ef122c32e05b33b1e3a7
Rahul Lohra <rahullohra987@gmail.com>
2022-07-06 12:31:29 +0530

    adjfksdj

--- PYTHON/rrrr.py
+++ PYTHON/rrrr.py
@@ -1,20 +1,14 @@
-class Solution:
-  def rotate(self, nums, k: int) -> None:
-    k %= len(nums)
-    self.reverse(nums, 0, len(nums) - 1)
-    self.reverse(nums, 0, k - 1)
-    self.reverse(nums, k, len(nums) - 1)
+def moveZeroes(nums):
+  i = len(nums) -1
+  if len(nums) <=1:
     return nums
 
-  def reverse(self, nums, l: int, r: int) -> None:
-    print('\n')
-    while l < r:
-      print(nums, l, r)
-      nums[l], nums[r] = nums[r], nums[l]
-      l += 1
-      r -= 1
-    return nums
+  while i >=0:
+    if nums[i] ==0:
+      del nums[i]
+      nums.append(0)
+    i-=1
 
-s = Solution()
-# print(s.rotate([1, 2, 3, 4, 5, 6, 7], 3))
-print(s.reverse([1, 2, 3, 4, 5, 6, 7], 0, 6))
+  return nums
+
+print(moveZeroes([0]))
