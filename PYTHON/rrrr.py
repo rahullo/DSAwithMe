83f063457d228a623c7367d8b16caf760dc1424c
Rahul Lohra <rahullohra987@gmail.com>
2022-07-06 12:16:53 +0530

    Hledkjf

--- PYTHON/rrrr.py
+++ PYTHON/rrrr.py
@@ -1,19 +1,20 @@
-n = 51
-bad = 10
+class Solution:
+  def rotate(self, nums, k: int) -> None:
+    k %= len(nums)
+    self.reverse(nums, 0, len(nums) - 1)
+    self.reverse(nums, 0, k - 1)
+    self.reverse(nums, k, len(nums) - 1)
+    return nums
 
-def isBadVersion(int):
-    if int == bad:
-        return True
-    else:
-        return False
+  def reverse(self, nums, l: int, r: int) -> None:
+    print('\n')
+    while l < r:
+      print(nums, l, r)
+      nums[l], nums[r] = nums[r], nums[l]
+      l += 1
+      r -= 1
+    return nums
 
-def firstBadVersion( n):
-        ans = False
-        while ans == False:
-            if isBadVersion(n) == True:
-                ans = True
-                return n
-            else:
-                n-=1
-
-print(firstBadVersion(n))
+s = Solution()
+# print(s.rotate([1, 2, 3, 4, 5, 6, 7], 3))
+print(s.reverse([1, 2, 3, 4, 5, 6, 7], 0, 6))
