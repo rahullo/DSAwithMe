0f9ec5c278c423c4d33557438c85a2bc3d539825
Rahul Lohra <rahullohra987@gmail.com>
2022-07-07 18:18:08 +0530

    reverse word in a string

--- PYTHON/rrrr.py
+++ PYTHON/rrrr.py
@@ -1,10 +1,24 @@
-from collections import deque
-# import queue
+def reverseString(s):
+    list1 = []
+    string = ' '
+    i = 0
+    l = len(s)
+    for item in s:
+        if item !=' ':
+            string +=item
+            i+=1
+            continue
+        if item == ' ':
+            # string+=' '
+            list1.append(string)
+            string = ' '
+            i+=1
+    list1.append(string)
+    for i in range(len(list1)):
+        ans = ''.join(reversed(list1[i]))
+        list1[i] = ans
+    print(list1)
+    return ''.join(list1)
 
-queue = deque(['rahul', 'chandan', 'yashwant'])
-print(queue)
-queue.append('gautam')
-print(queue)
-fifo = queue.popleft()
-print(fifo)
-print(queue)
+
+print(reverseString( "Let's take LeetCode contest"))
