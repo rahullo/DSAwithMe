98eec1045c6dcaf9c660678a59c0b266cb863aa7
Rahul Lohra <rahullohra987@gmail.com>
2022-07-07 18:38:02 +0530

    asdfh

--- PYTHON/rrrr.py
+++ PYTHON/rrrr.py
@@ -1,24 +1,25 @@
 def reverseString(s):
-    list1 = []
-    string = ' '
-    i = 0
-    l = len(s)
-    for item in s:
-        if item !=' ':
-            string +=item
-            i+=1
-            continue
-        if item == ' ':
-            # string+=' '
-            list1.append(string)
-            string = ' '
-            i+=1
-    list1.append(string)
-    for i in range(len(list1)):
-        ans = ''.join(reversed(list1[i]))
-        list1[i] = ans
-    print(list1)
-    return ''.join(list1)
+    # list1 = []
+    # string = ' '
+    # i = 0
+    # l = len(s)
+    # for item in s:
+    #     if item !=' ':
+    #         string +=item
+    #         i+=1
+    #         continue
+    #     if item == ' ':
+    #         # string+=' '
+    #         list1.append(string)
+    #         string = ' '
+    #         i+=1
+    # list1.append(string)
+    # for i in range(len(list1)):
+    #     ans = ''.join(reversed(list1[i]))
+    #     list1[i] = ans
+    # print(list1)
+    # return ''.join(list1)
+    return " ".join([word[::-1] for word in s.split()])
 
 
 print(reverseString( "Let's take LeetCode contest"))
