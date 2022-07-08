6e557d1f449929e3840cceffcad6bdcbf5743d37
Rahul Lohra <rahullohra987@gmail.com>
2022-07-08 12:20:37 +0530

    adjfkaj

--- PYTHON/rrrr.py
+++ PYTHON/rrrr.py
@@ -1,70 +1,14 @@
-class Node:
-  def __init__(self, val=0, next=None):
-    self.val = val
-    self.next = next
+from audioop import reverse
 
 
-class Linked_List:
-  def __init__(self):
-    self.head = None
-    self.tail = self.head
-    self.length = 0
+def findMedianSortedArrays(nums1, nums2):
+  merged = nums1 + nums2
+  merged.sort(reverse=False)
+  lenMerged = len(nums1)+len(nums2)
+  mid = lenMerged//2
+  if lenMerged % 2 == 0:
+    return ( merged[mid-1] + merged[mid])/2
+  else:
+    return merged[mid]
 
-  def append(self, value):
-    new_node = Node(value)
-    if self.head == None:
-      self.head = new_node
-      self.tail = new_node
-      self.length = 1
-    else:
-      self.tail.next = new_node
-      self.tail = new_node
-      self.length +=1
-
-
-  def printList(self, head):
-    curr = head
-    while curr != None:
-      print(curr.val, end='-> ')
-      curr = curr.next
-  
-link = Linked_List()
-link.append(1)
-link.append(2)
-# link.append(3)
-# link.append(4)
-# link.append(5)
-# link.append(6)
-# link.append(7)
-link.printList(link.head)
-
-def middleNode(head, n):
-  if head.next == None:
-            head = None
-            return head
-  node = head
-  prevNode = None
-  x = Count(head)
-  if x == n:
-    head = head.next
-    return head
-  i = 0
-  while i != (x - (n-1)-1):
-    prevNode = node
-    node = node.next
-    i+=1
-  prevNode.next = node.next
-  node.next = None
-  return head
-
-def Count(head):
-  i = 0
-  node = head
-  while node != None:
-    i +=1
-    node = node.next
-  return i
-  
-print('\n')
-head = middleNode(link.head, 2)
-link.printList(head)
+print(findMedianSortedArrays([1, 2], [3, 4]))
