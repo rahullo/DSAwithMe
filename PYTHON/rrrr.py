dc4a912d62b66a582ac384cb54f29236c2c7537e
Rahul Lohra <rahullohra987@gmail.com>
2022-07-10 22:36:33 +0530

    asdjfas

--- PYTHON/rrrr.py
+++ PYTHON/rrrr.py
@@ -1,23 +1,17 @@
-def floodfill(image, sr, sc, newColor):
-  startcolor = image[sr][sc]
-  seen = set()
+def maxArea(grid):
+  def dfs(i: int, j: int) -> int:
+      if i < 0 or i == len(grid) or j < 0 or j == len(grid[0]):
+        return 0
+      if grid[i][j] != 1:
+        return 0
 
-  def dfs(i, j):
-    if i < 0 or i==len(image) or j < 0 or j == len(image):
-      return
-    if image[i][j] != startcolor or (i, j) in seen:
-      return
-     
-    image[i] [j] = newColor
-    seen.add((i, j))
-    dfs(i+1, j)
-    dfs(i-1, j)
-    dfs(i, j+1)
-    dfs(i, j-1)
+      grid[i][j] = 2
 
-  dfs(sr, sc)
-  return image
+      return 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1)
+
+  return max(dfs(i, j) for i in range(len(grid)) for j in range(len(grid[0])))
 
 
-print(floodfill([[0, 0, 0], [0, 0, 0]], 0, 0, 0))
-print(floodfill([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2))
+
+print(maxArea([[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]))
+print(maxArea([[0,0,0,0,0,0,0,0]]))
